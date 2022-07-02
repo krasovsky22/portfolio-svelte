eb40a766d8b59e96087e2558b9a6d73ff673c2e7
Vlad Krasovsky <krasovsky@kentik.com>
2022-07-03 00:13:39 +0800

    fixed badge

--- src/components/badges/AWSSolutionArchitect.svelte
+++ src/components/badges/AWSSolutionArchitect.svelte
@@ -2,7 +2,7 @@
 	export let width: number = 75;
 </script>
 
-<div class="badges">
+<div class="w-fit">
 	<a
 		href="https://www.credly.com/badges/32ec4269-6de1-40cf-87e2-7a8983182b38/public_url"
 		target="_blank"
