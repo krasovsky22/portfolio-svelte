148c860626b24a9e503609d180b1a5ca33880691
Vlad Krasovski <krasovsky@kentike.com>
2025-05-18 12:35:26 +0800

    remove view from aws solution architect card

--- src/components/badges/AWSSolutionArchitect.svelte
+++ src/components/badges/AWSSolutionArchitect.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-    import HoverableCard from '@/components/layout/HoverableCard.svelte';
-
-    const URL = 'https://www.credly.com/badges/32ec4269-6de1-40cf-87e2-7a8983182b38/public_url';
-
-    const handleOnClick = () => {
-        window.open(URL, '_blank');
-    }
+	import HoverableCard from '@/components/layout/HoverableCard.svelte';
 </script>
 
-<HoverableCard imageUrl="/badges/aws-certified-solutions-architect-associate.png" onClickEvent={handleOnClick} />
+<HoverableCard imageUrl="/badges/aws-certified-solutions-architect-associate.png" />
